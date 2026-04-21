942f503d1256fb3ec09c04207b4b83760e7b2c62
Jgziin <jorgehenrique.miranda20@gmail.com>
2026-04-21 15:37:52 -0300

    a

---
+++ adicionais/adicional.js
@@ -0,0 +1,18 @@
+//Operadores Lógicos
+
+//&& = Lido como "E", somente retorna True caso as duas condições sejam verdadeiras (no caso iguais), caso contrário retornará False.
+
+//|| = Lido como "Ou", retorna True caso uma das condições sejam verdadeiras, apenas retornará False, caso ambas sejam falsas.
+
+//A ordem dos operadores lógicos em uma expressão se dá
+
+//1- !
+// 2- &&
+// 3- ||
+
+//Operadores Ternários ( ? : )
+
+// /Eles tem três operandos, podem ser lidos como:
+
+// ? = "Se sim.." Caso a expressão seja verdadeira.
+// : = "Se não..." Caso a expressão seja falsa.
