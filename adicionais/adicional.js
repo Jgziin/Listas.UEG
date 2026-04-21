e49793c123affaadf36a8e3aee7247bc845efea4
Jgziin <jorgehenrique.miranda20@gmail.com>
2026-04-21 18:21:16 -0300

    Update adicional.js

--- adicionais/adicional.js
+++ adicionais/adicional.js
@@ -13,13 +13,20 @@
 
 
 let prompt = require('prompt-sync')();
-let numero;
-while(numero != 200)  {
-    numero = parseInt(prompt("Digite um número: "));
-    if(numero == 200) {
-        console.log("Parabéns, você acertou o número!");
-    } else {
-        console.log("Tente novamente!");
 
+function flamengo() {
+    let time = prompt('Qual é o melhor time do mundo? ');
+    let titulos = parseInt(prompt('Quantos títulos o Flamengo tem? '));
+    let resposta = (time == 'Flamengo' && titulos <= 5) ? 'Resposta correta!' 
+    : 'Resposta incorreta! O melhor time do mundo é o Flamengo e ele tem 4 títulos da Libertadores e 1 títulos do Mundial de Clubes, totalizando 5 títulos internacionais!';
+    if (resposta == 'Resposta correta!') {
+        console.log(resposta);
+    }
+    else {
+        console.log(resposta);
     }
 }
+console.log('Vamos testar seus conhecimentos sobre o Flamengo!');
+
+
+flamengo();
